2e262191c8a006b8d03a2255507647b00741f3ab
gunawanbudiono <gunawanbudiono@gmail.com>
2026-08-25 21:35:25 +0700

    Test: Add container pot verification script

---
+++ scratch/test_container_pot.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const { execFile } = require('child_process');
+
+function checkPing(url) {
+  return new Promise((resolve) => {
+    http.get(url, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => resolve({ status: res.statusCode, data }));
+    }).on('error', err => resolve({ error: err.message }));
+  });
+}
+
+async function run() {
+  console.log('1. PING 172.17.0.1:4416:', await checkPing('http://172.17.0.1:4416/ping'));
+  console.log('2. PING 192.168.18.2:4416:', await checkPing('http://192.168.18.2:4416/ping'));
+
+  const args = [
+    '-m', 'yt_dlp',
+    '--extractor-args', 'youtubepot-bgutilhttp:base_url=http://172.17.0.1:4416',
+    '--dump-single-json',
+    '--no-warnings',
+    '--skip-download',
+    'https://www.youtube.com/watch?v=n7X2cbKzh-Q'
+  ];
+
+  execFile('python3', args, { maxBuffer: 10 * 1024 * 1024 }, (err, stdout, stderr) => {
+    if (err) {
+      console.log('3. YT-DLP ERROR:', stderr || err.message);
+    } else {
+      try {
+        const info = JSON.parse(stdout);
+        console.log('3. YT-DLP SUCCESS! Title:', info.title);
+      } catch (e) {
+        console.log('3. YT-DLP PARSE ERR:', stdout.slice(0, 200));
+      }
+    }
+  });
+}
+
+run();
