6dcef674e872761ee74ca926366d83b4bad16a7d
gunawanbudiono <gunawanbudiono@gmail.com>
2026-08-25 21:36:20 +0700

    Fix: Await execFile in test_container_pot.js

--- scratch/test_container_pot.js
+++ scratch/test_container_pot.js
@@ -24,17 +24,20 @@ async function run() {
     'https://www.youtube.com/watch?v=n7X2cbKzh-Q'
   ];
 
-  execFile('python3', args, { maxBuffer: 10 * 1024 * 1024 }, (err, stdout, stderr) => {
-    if (err) {
-      console.log('3. YT-DLP ERROR:', stderr || err.message);
-    } else {
-      try {
-        const info = JSON.parse(stdout);
-        console.log('3. YT-DLP SUCCESS! Title:', info.title);
-      } catch (e) {
-        console.log('3. YT-DLP PARSE ERR:', stdout.slice(0, 200));
+  await new Promise((resolve) => {
+    execFile('python3', args, { maxBuffer: 10 * 1024 * 1024 }, (err, stdout, stderr) => {
+      if (err) {
+        console.log('3. YT-DLP ERROR:', stderr || err.message);
+      } else {
+        try {
+          const info = JSON.parse(stdout);
+          console.log('3. YT-DLP SUCCESS! Title:', info.title);
+        } catch (e) {
+          console.log('3. YT-DLP PARSE ERR:', stdout.slice(0, 200));
+        }
       }
-    }
+      resolve();
+    });
   });
 }
 
