4572e2afc176022d941ea6b38e2ed05bd64001f9
gunawanbudiono <gunawanbudiono@gmail.com>
2026-08-25 22:21:51 +0700

    Test: player_client=android,ios,web_safari

--- scratch/test_container_pot.js
+++ scratch/test_container_pot.js
@@ -18,7 +18,7 @@ async function run() {
   const args = [
     '-m', 'yt_dlp',
     '--cookies', '/app/db/cookies.txt',
-    '--extractor-args', 'youtubepot-bgutilhttp:base_url=http://multistream-pot-provider:4416;youtube:player_client=web,web_creator,android',
+    '--extractor-args', 'youtubepot-bgutilhttp:base_url=http://multistream-pot-provider:4416;youtube:player_client=android,ios,web_safari',
     '--dump-single-json',
     '--no-warnings',
     '--skip-download',
